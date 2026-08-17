ad3eb4bbb2bd13319e9e1fa4347e772f721a6fd3
Kauã Abreu <kaua808player@gmail.com>
2026-08-17 10:30:06 -0300

    script com Análise de dados – tabela de frequências

--- script_turismo.R
+++ script_turismo.R
@@ -7,3 +7,9 @@ summary(dados)
 # Atribuindo legendas dos rótulos das variáveis sexo e estabelecimento
 dados$sexo = factor(dados$sexo, levels = c(1, 2), labels = c("masculino", "feminino"))
 dados$estabelecimento = factor(dados$estabelecimento, levels = c("P", "H"), labels = c("pousada", "hotel"))
+
+# Análise de dados
+# Tabelas de distribuição de frequências das variáveis sexo e estabelecimento
+round(prop.table(table(dados$sexo)) * 100, 2)
+round(prop.table(table(dados$estabelecimento)) * 100, 2)
+round(prop.table(table(dados$sexo, dados$estabelecimento), margin = 2) * 100, 2)
